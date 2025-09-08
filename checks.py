a68ed8df137658eaa59fc1ac7d6e018ecbf913e7
JuPetr <106886851+Harvar3105@users.noreply.github.com>
2025-09-08 11:14:42 +0000

    abc

--- checks.py
+++ checks.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
-
-ADMIN_IDS = [123456789012345678, 987654321098765432]  # user IDs
+from config import ADMIN_IDS
 
 def is_admin():
     async def predicate(ctx: commands.Context):
