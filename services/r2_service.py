0861b9fa6a6491ea5467f2e5e2818fc84ea2bdf1
JuPetr <106886851+Harvar3105@users.noreply.github.com>
2025-09-22 10:09:09 +0000

    try fix

--- services/r2_service.py
+++ services/r2_service.py
@@ -2,18 +2,20 @@ import aioboto3
 import os
 
 async def s3_client():
-    return aioboto3.client(
-        service_name='s3',
-        aws_access_key_id=os.getenv("R2_ACCESS_KEY"),
-        aws_secret_access_key=os.getenv("R2_SECRET_KEY"),
-        endpoint_url=os.getenv("R2_ENDPOINT")
-    )
+  return aioboto3.client(
+    service_name='s3',
+    aws_access_key_id=os.getenv("R2_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("R2_SECRET_KEY"),
+    endpoint_url=os.getenv("R2_ENDPOINT")
+  )
 
 async def list_files():
-    async with await s3_client() as s3:
-        response = await s3.list_objects_v2(Bucket=os.getenv("R2_BUCKET"))
-        return [obj['Key'] for obj in response.get('Contents', [])]
+  async with await s3_client() as s3:
+    print(Bucket=os.getenv("R2_BUCKET"))
+    response = await s3.list_objects_v2(Bucket=os.getenv("R2_BUCKET"))
+    print(response)
+    return [obj['Key'] for obj in response.get('Contents', [])]
 
 def get_file_url(file_name):
-    return f"{os.getenv('R2_ENDPOINT')}/{os.getenv('R2_BUCKET')}/{file_name}"
+  return f"{os.getenv('R2_ENDPOINT')}/{os.getenv('R2_BUCKET')}/{file_name}"
 
