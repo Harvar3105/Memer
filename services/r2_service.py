f9afd7ef7cb763e00fce11fc33f16c7fe4108996
JuPetr <106886851+Harvar3105@users.noreply.github.com>
2025-09-25 11:04:30 +0000

    some changes

--- services/r2_service.py
+++ services/r2_service.py
@@ -1,20 +1,43 @@
 import aioboto3
 import os
 
-async def s3_client():
-  return aioboto3.client(
-    service_name='s3',
-    aws_access_key_id=os.getenv("R2_ACCESS_KEY"),
-    aws_secret_access_key=os.getenv("R2_SECRET_KEY"),
-    endpoint_url=os.getenv("R2_ENDPOINT")
-  )
+# def s3_client():
+#   return aioboto3.client(
+#     service_name='s3',
+#     aws_access_key_id=os.getenv("R2_ACCESS_KEY"),
+#     aws_secret_access_key=os.getenv("R2_SECRET_KEY"),
+#     endpoint_url=os.getenv("R2_ENDPOINT")
+#   )
+
+s3_client = aioboto3.Session(
+  aws_access_key_id=os.getenv("R2_ACCESS_KEY"),
+  aws_secret_access_key=os.getenv("R2_SECRET_KEY"),
+  region_name="auto"
+).client(
+    service_name="s3",
+    endpoint_url=f"{os.getenv("R2_ENDPOINT")}/{os.getenv("R2_BUCKET")}"
+    )
 
 async def list_files():
-  async with await s3_client() as s3:
-    print(Bucket=os.getenv("R2_BUCKET"))
-    response = await s3.list_objects_v2(Bucket=os.getenv("R2_BUCKET"))
-    print(response)
-    return [obj['Key'] for obj in response.get('Contents', [])]
+  print("Listing files start")
+  print(s3_client)
+  print(os.getenv("R2_ENDPOINT"))
+  print(os.getenv("R2_BUCKET"))
+  async with s3_client as s3:
+    print("Requesting response")
+    paginator = s3.get_paginator("list_objects_v2")
+    print(paginator)
+
+    files = []
+    async for page in paginator.paginate(Bucket=os.getenv("R2_BUCKET")):
+        for obj in page.get("Contents", []):
+            files.append(obj["Key"])
+
+    return files
+
+    # response = await s3.list_objects_v2()
+    # print(response)
+    # return [obj['Key'] for obj in response.get('Contents', [])]
 
 def get_file_url(file_name):
   return f"{os.getenv('R2_ENDPOINT')}/{os.getenv('R2_BUCKET')}/{file_name}"
